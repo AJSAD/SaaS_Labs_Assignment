cf81a0fe3331fe7eae60a55766df1a77f149b64f
Ajay Daniel <ajay.daniel@esko.com>
2024-12-22 23:17:41 +0530

    Populate table contents with pagination

---
+++ src/Utils/constants.js
@@ -0,0 +1,28 @@
+// Table Container
+export const FETCH_URL = "https://raw.githubusercontent.com/saaslabsco/frontend-assignment/refs/heads/master/frontend-assignment.json";
+export const ERROR_MSG = "Error fetching data:";
+export const CURRENT_PAGE = "currentPage";
+export const TOTAL_PAGE = "totalPage";
+export const SNO_LABEL = "S.No.";
+export const PERCENTAGE_FUNDED_LABEL = "Percentage funded";
+export const AMOUNT_PLEDGED_LABEL = "Amount pledged";
+export const BY_KEY = "by";
+export const SNO_KEY = "s.no";
+export const PERCENTAGE_FUNDED_KEY = "percentage.funded";
+export const AMOUNT_PLEDGED_KEY = "amt.pledged";
+
+// Pagination
+export const PREVIOUS = "Previous";
+export const PAGE = "Page";
+export const NEXT = "Next";
+
+// General
+export const VALUE = "target.value";
+export const KEY = "key";
+export const ENTER = "Enter";
+export const HIFEN = "-";
+export const NUMBER = "number";
+
+
+
+
